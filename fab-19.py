37302f36e2f7e387edff19ef921a2586d7675a0f
unknown <muhammadismailsoomro09@gmail.com>
2022-02-19 17:51:57 +0500

    second commit

---
+++ fab-19.py
@@ -0,0 +1,52 @@
+# # operators in python
+
+# # 2^2 = 2*2
+# # 2^8 = 2*2*2*2*2*2*2*2 = 256
+
+# print(2**8) 
+# print(5/3) # 1.5 float
+# print(5//2) # int
+# print(5%3)
+
+# _________________________________________________
+
+# name , age = input("Enter your name and age ").split()
+# print(f"Hello {name}, you are {age} years old")
+
+# num_one, operator, num_two = input().split()
+# if operator == "+":
+#     print(int(num_one) + int(num_two))
+# elif operator == "-":
+#     print(int(num_one) - int(num_two))
+# elif operator == "*":
+#     print(int(num_one) * int(num_two))
+# elif operator == "/":
+#     print(int(num_one) / int(num_two))
+# count_latter = dict()
+# temp = ""
+# n = "abdul qudoos"
+# for i in range(0,len(n)):
+#     if n[i] not in temp:
+#         total = n.count(n[i])
+#         count_latter[n[i]] = total
+#         temp += n[i]
+# for key, value in count_latter.items():
+#     print(f"{key} : {value}")
+
+#              -1,-2,-3
+# #    0123456789
+# n = "abDul AuDoos"
+# # capitalize()
+# print(n.capitalize())
+# print(n.upper())
+# print((n.lower()))
+
+# print("hello\nclass")
+# print("hello ", end="")
+# print(" class", end="")
+
+# name = "10"
+# name_s = "Saboor Rao"
+
+# print(name - name_s)
+
