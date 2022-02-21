43de0775ff26a557d5e4f1dd64b8889197d28ceb
unknown <muhammadismailsoomro09@gmail.com>
2022-02-21 18:11:28 +0500

    21 fab

---
+++ fab-21.py
@@ -0,0 +1,78 @@
+# list_one = [2,4,6,7,8]
+# list_two = [1,3,4,5,7]
+# i = 0
+
+# while i <= 4: #no of ittration 5   ---- [0,1,2,3,4]
+#     print(list_one[i] * list_two[i])
+#     i += 1
+#     # i = i+1    
+# _________________________________________________
+
+# for i in range(0,5):
+#     print(list_one[i]*list_two[i])
+# _________________________________________________
+# j = -1
+# for i in range(0,5):
+#     print(list_one[i] * list_two[j])
+#     j -= 1
+#     # j = j-1
+# ________________________________________________
+# i = 0
+# j = -1
+# while i <=4:
+#     print(list_one[i]*list_two[j])
+#     i +=1
+#     j -=1 
+
+# looping
+# for i in range(0,3):
+#     for j in range(0,3):
+        # print(f"i = {i} , j = {j}")
+        # print("*")
+# ________________________________________________
+
+# list comprihenson
+# list_one = []
+# list_one.append(20)
+# list_one.append(21)
+# print(list_one)
+# list_one.insert(0, 2)
+# print(list_one)
+# list_one.append(40)
+# print(list_one)
+# # [1,2,3, ... , 20]
+# for i in range(0,21):
+#     if i%2==0:
+#         list_one.append(i)
+# print(list_one)
+
+# list_two = [i for i in range(3,31) if i%3==0]
+# list_two = [i for i in range(3,31,3)]
+# print(list_two)
+# _________________________________________
+# squares = []
+# # for i in range(1,101):
+# # #    # squares.append(i*i)
+# #     squares.append(i**2)
+# print(squares)
+# _____________________________________
+# squares = [i*i for i in range(1,101)]
+# squares = [i**2 for i in range(1,101)]
+# print(squares)
+
+# # [1,4,9,15 .. 100]
+
+#%
+# //
+# a = 99812
+# print(a//10)
+
+
+
+
+
+
+
+
+
+
