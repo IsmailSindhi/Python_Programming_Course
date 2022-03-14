45b555b9cd3fb844e134cbe00ac3c8bc9ec46dce
unknown <muhammadismailsoomro09@gmail.com>
2022-03-14 18:16:01 +0500

    updated

---
+++ mar-12.py
@@ -0,0 +1,19 @@
+# # Function
+# # A peace of code which perform some task
+# # 
+
+
+# # lambda expression:
+
+# # def add(a,b):
+# #     return a+b
+
+# # function defination with lambda
+# func = lambda a,b : True if a>b else False
+# func = lambda a,b : a>b
+# add2 = lambda a,b : a+b
+# square = lambda a:a*b
+# square = lambda a:a**2
+# # function call
+# # print(add2(2,3))
+# print(square(4))
